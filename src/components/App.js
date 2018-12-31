20bf3ee0a353f6ac966a510db571b64bb9d07fc6
emile <emile31.roch@gmail.com>
2018-12-31 13:38:15 +0100

    Add routing to the project #6

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,32 @@ import React from "react";
 import data from "../data/project.json";
 import List from "./List/List";
 import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-let App = () => {
-  return (
+const App = () => (
+  <Router>
     <div>
-      <List elts={data} />
+      <Header />
+
+      <Route exact path="/" component={() => <List elts={data} />} />
+      <Route path="/about" component={About} />
+      <Route path={`/project/:id`} component={Project} />
     </div>
-  );
-};
+  </Router>
+);
+
+const About = () => <h2>About</h2>;
+const Project = ({ match }) => <h3>Project : {match.params.id}</h3>;
+
+const Header = () => (
+  <ul>
+    <li>
+      <Link to="/">All projects</Link>
+    </li>
+    <li>
+      <Link to="/about">About</Link>
+    </li>
+  </ul>
+);
 
 export default App;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { compose, mapProps, withState /* ... */ } from "recompose";
+import { Link } from "react-router-dom";
 import "./List.css";
 
 let List = ({ elts }) => {
   return (
     <div className="list">
       {elts.map((elt, index) => {
-        return <div key={index}>{elt.title}</div>;
+        return (
+          <div key={index}>
+            <Link to={`/project/${elt.title}`}>{elt.title}</Link>
+          </div>
+        );
       })}
     </div>
   );
